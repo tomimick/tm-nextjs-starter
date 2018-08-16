5f72b31b5b3a8171bbcd55817042055a7e25479e
Tomi Mickelsson <atomi@iki.fi>
2018-08-16 09:52:08 +0300

    Export: no site prefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 
+/* Configuration to Next.js
+ * - assetPrefix can be used to set a prefix for the site, but it was not
+ *   enough, the links+ajax calls would also need the prefix, did not go
+ *   that route */
+
 const withSass = require('@zeit/next-sass')
 
-const isProd = process.env.NODE_ENV === 'production'
+//const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withSass({
-    cssModules: false,
-    assetPrefix: isProd ? '/tm-nextjs-starter' : ''
+    cssModules: false
+//    assetPrefix: isProd ? '/tm-nextjs-starter' : ''
 })
 
